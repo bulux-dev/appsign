49f4d4713d1e3ac99fe1b61cee1f68dea3b01803
bulux-dev <daniel.bulux@hotmail.com>
2025-07-29 23:30:10 -0600

    cambios para firebase
    aun hace falta la revision de los mismos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:appsign/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:appsign/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
